cc47a0f354908d7430440eb4453326f50f873514
DoDucViet <doducviet.brse@outlook.com>
2017-06-04 19:07:39 +0900

    Java . Hello GitHub . First commit
    Java . Hello GitHub . First commit

---
+++ src/HelloGitHub.java
@@ -0,0 +1,19 @@
+
+/**
+ * [Class description]
+ *
+ * @author DoDucViet
+ * @version $Revision$
+ */
+public class HelloGitHub {
+
+    /**
+     * [method description]
+     *
+     * @author DoDucViet
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
