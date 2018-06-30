8821766d7c6be26023bccb264f1e92367018a00f
Do Duc Viet <>
2018-06-30 11:44:47 +0900

    Hello SourceTree

---
+++ src/SourceTree.java
@@ -0,0 +1,19 @@
+
+/**
+ * [Class description]
+ *
+ * @author DoDucViet
+ * @version $Revision$
+ */
+public class SourceTree {
+
+    /**
+     * [method description]
+     *
+     * @author DoDucViet
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello SourceTree!");
+    }
+}
